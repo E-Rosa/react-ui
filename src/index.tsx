335bee0c30ebb7ef48c30f3a68863d8eff591376
Elias <elias_rrosa@hotmail.com>
2023-06-07 09:39:44 -0300

    imported standards.css

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import { Carousel, CarouselContext } from "./carousel";
 import { Modal, ModalContext } from "./modal";
 import { FeedbackProvider, FeedbackProviderContext } from "./feedbackProvider";
 import "./index.css";
+import "./standards.css"
 
 export {
   Carousel,
