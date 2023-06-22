b0df3532f215b090ca9dd39384d332033541ca52
Elias <elias_rrosa@hotmail.com>
2023-06-22 06:36:28 -0300

    fix: feedbackprovider fixed position

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
+import { FeedbackProvider } from ".";
 import "./App.css";
 import "./standards.css";
-import { useState } from "react";
-
+import { createContext, useState } from "react";
 
 function App() {
-  const [isActive, setIsActive] = useState(true)
+  const [isActive, setIsActive] = useState(true);
+  const feedbackContext = createContext
   return (
     <div className="halfscreen">
-      
     </div>
-
   );
 }
 
